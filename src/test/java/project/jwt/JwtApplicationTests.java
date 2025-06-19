2b68b16331e80c8a2b8d6feb5ef97ecd1afe2875
cherish0-0 <jiyu01160@naver.com>
2025-06-19 21:48:16 +0900

    Initial commit

---
+++ src/test/java/project/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package project.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
