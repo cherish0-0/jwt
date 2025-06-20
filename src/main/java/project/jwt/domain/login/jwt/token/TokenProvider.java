3a4c4f38b487bd996672ea3d071e7d8b3beb920e
cherish0-0 <jiyu01160@naver.com>
2025-06-20 22:26:24 +0900

    refactor: Duration 사용해서 코드 개선

--- src/main/java/project/jwt/domain/login/jwt/token/TokenProvider.java
+++ src/main/java/project/jwt/domain/login/jwt/token/TokenProvider.java
@@ -10,74 +10,74 @@ import project.jwt.domain.login.dto.TokenValidationResult;
 import project.jwt.domain.member.Member;
 
 import java.security.Key;
-import java.time.Duration;
+import java.time.*;
 import java.util.Date;
 import java.util.UUID;
 
 @Slf4j
 public class TokenProvider {
 
-    private static final String AUTHORITIES_KEY = "auth";
-    private static final String TOKEN_ID_KEY = "tokenId";
-    private static final String USERNAME_KEY = "username";
+	private static final String AUTHORITIES_KEY = "auth";
+	private static final String TOKEN_ID_KEY = "tokenId";
+	private static final String USERNAME_KEY = "username";
 
-    private final Key hashKey;
-    private final long accessTokenValidationInSeconds;
+	private final Key hashKey;
+	private final Duration SecondsToAdd;
 
-    public TokenProvider (String secrete, long accessTokenValidationInSeconds) {
-        byte[] keyBytes = Decoders.BASE64.decode(secrete);
-        this.hashKey = Keys.hmacShaKeyFor(keyBytes);
-        this.accessTokenValidationInSeconds = accessTokenValidationInSeconds * 1000;
-    }
+	public TokenProvider(String secrete, Duration SecondsToAdd) {
+		byte[] keyBytes = Decoders.BASE64.decode(secrete);
+		this.hashKey = Keys.hmacShaKeyFor(keyBytes);
+		this.SecondsToAdd = SecondsToAdd;
+	}
 
-    public TokenInfo createToken(Member member) {
-        long currentTime = (new Date()).getTime();
-        Date accessTokenExpireTime = new Date(currentTime + accessTokenValidationInSeconds);
-        String tokenId = UUID.randomUUID().toString();
+	/**
+	 * JWT 토큰 생성
+	 * - setSubject: 토큰 페이로드(속성)의 주체 설정
+	 * - claim: 페이로드의 속성 설정 (키, 값 쌍으로 저장)
+	 * - compact: JWT 토큰 문자열로 변환
+	 */
+	public TokenInfo createToken(Member member) {
+		String tokenId = UUID.randomUUID().toString();
+		Date issuedAt = Date.from(Instant.now());
+		Date expiration = Date.from(issuedAt.toInstant().plus(SecondsToAdd));
 
-        /**
-         * JWT 토큰 생성
-         * - setSubject: 토큰 페이로드(속성)의 주체 설정
-         * - claim: 페이로드의 속성 설정 (키, 값 쌍으로 저장)
-         * - compact: JWT 토큰 문자열로 변환
-         */
-        String accessToken = Jwts.builder()
-                .setSubject(member.getEmail())
-                .claim(AUTHORITIES_KEY, member.getRole())
-                .claim(USERNAME_KEY, member.getUsername())
-                .claim(TOKEN_ID_KEY, tokenId)
-                .signWith(hashKey, SignatureAlgorithm.HS512)
-                .setIssuedAt(new Date(currentTime))
-                .setExpiration(accessTokenExpireTime)
-                .compact();
+		String accessToken = Jwts.builder()
+			.setSubject(member.getEmail())
+			.claim(AUTHORITIES_KEY, member.getRole())
+			.claim(USERNAME_KEY, member.getUsername())
+			.claim(TOKEN_ID_KEY, tokenId)
+			.signWith(hashKey, SignatureAlgorithm.HS512)
+			.setIssuedAt(issuedAt)
+			.setExpiration(expiration)
+			.compact();
 
-        return TokenInfo.builder()
-                .ownerEmail(member.getEmail())
-                .tokenId(tokenId)
-                .accessToken(accessToken)
-                .accessTokenExpireTime(accessTokenExpireTime)
-                .build();
-    }
+		return TokenInfo.builder()
+			.ownerEmail(member.getEmail())
+			.tokenId(tokenId)
+			.accessToken(accessToken)
+			.accessTokenExpireTime(expiration)
+			.build();
+	}
 
-    public TokenValidationResult validateToken(String token) {
-        try {
-            Claims claims = Jwts.parserBuilder().setSigningKey(hashKey).build().parseClaimsJws(token).getBody();
-            return new TokenValidationResult(TokenStatus.TOKEN_VALID, TokenType.ACCESS,
-                    claims.get(TOKEN_ID_KEY, String.class), claims);
-        } catch (ExpiredJwtException e) {
-            log.info("만료된 JWT 토큰");
-            Claims claims = e.getClaims();
-            log.error("issue: {}, expiration: {}", claims.getIssuedAt(), claims.getExpiration());
-            return new TokenValidationResult(TokenStatus.TOKEN_EXPIRED, TokenType.ACCESS, claims.get(TOKEN_ID_KEY, String.class), null);
-        } catch (SecurityException | MalformedJwtException e) {
-            log.info("잘못된 JWT 서명");
-            return new TokenValidationResult(TokenStatus.TOKEN_WRONG_SIGNATURE, null, null, null);
-        } catch (UnsupportedJwtException e) {
-            log.info("지원되지 않는 JWT 서명");
-            return new TokenValidationResult(TokenStatus.TOKEN_HASH_NOT_SUPPORTED, null, null, null);
-        } catch (IllegalArgumentException e) {
-            log.info("잘못된 JWT 토큰");
-            return new TokenValidationResult(TokenStatus.TOKEN_WRONG_SIGNATURE, null, null, null);
-        }
-    }
+	public TokenValidationResult validateToken(String token) {
+		try {
+			Claims claims = Jwts.parserBuilder().setSigningKey(hashKey).build().parseClaimsJws(token).getBody();
+			return new TokenValidationResult(TokenStatus.TOKEN_VALID, TokenType.ACCESS,
+				claims.get(TOKEN_ID_KEY, String.class), claims);
+		} catch (ExpiredJwtException e) {
+			log.info("만료된 JWT 토큰");
+			Claims claims = e.getClaims();
+			return new TokenValidationResult(TokenStatus.TOKEN_EXPIRED, TokenType.ACCESS,
+				claims.get(TOKEN_ID_KEY, String.class), null);
+		} catch (SecurityException | MalformedJwtException e) {
+			log.info("잘못된 JWT 서명");
+			return new TokenValidationResult(TokenStatus.TOKEN_WRONG_SIGNATURE, null, null, null);
+		} catch (UnsupportedJwtException e) {
+			log.info("지원되지 않는 JWT 서명");
+			return new TokenValidationResult(TokenStatus.TOKEN_HASH_NOT_SUPPORTED, null, null, null);
+		} catch (IllegalArgumentException e) {
+			log.info("잘못된 JWT 토큰");
+			return new TokenValidationResult(TokenStatus.TOKEN_WRONG_SIGNATURE, null, null, null);
+		}
+	}
 }
