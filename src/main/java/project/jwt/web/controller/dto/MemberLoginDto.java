361327ac0286a921ae5178b4e3b1fb8b57fa889b
cherish0-0 <jiyu01160@naver.com>
2025-06-21 17:27:02 +0900

    feat: 로그인 API 구현

---
+++ src/main/java/project/jwt/web/controller/dto/MemberLoginDto.java
@@ -0,0 +1,19 @@
+package project.jwt.web.controller.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberLoginDto {
+	@Email
+	private String email;
+
+	@NotBlank
+	private String password;
+}
